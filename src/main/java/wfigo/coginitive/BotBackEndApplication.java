fd23942d04c1346d35cd577c19e7637e28488286
wfigo7 <wfigo.7@gmail.com>
2017-12-10 23:44:49 +0900

    project to spring boot application

---
+++ src/main/java/wfigo/coginitive/BotBackEndApplication.java
@@ -0,0 +1,12 @@
+package wfigo.coginitive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotBackEndApplication.class, args);
+	}
+}
---
+++ src/test/java/wfigo/coginitive/BotBackEndApplicationTests.java
@@ -0,0 +1,16 @@
+package wfigo.coginitive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BotBackEndApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
