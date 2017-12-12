2dea9d8e1ba84fff60bcb90c80a8ef8efb03963a
wfigo7 <wfigo.7@gmail.com>
2017-12-12 23:54:34 +0900

    スタンプのpackageIdに応じて定義済みスタンプを返す

--- src/main/java/wfigo/coginitive/controller/LineBotController.java
+++ src/main/java/wfigo/coginitive/controller/LineBotController.java
@@ -1,7 +1,10 @@
 package wfigo.coginitive.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.google.common.collect.ImmutableMap;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.StickerMessageContent;
@@ -21,19 +24,29 @@ public class LineBotController {
 	@Autowired
 	SlackWebhookService slackService;
 
+	private Map<String, String> stickerMap = ImmutableMap.of("1", "13", "2", "175", "3", "191", "4", "276");
+
 	@EventMapping
 	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
 		log.info("event: " + event);
 		slackService.sendTextMessage(event.getMessage().getText());
 		return new TextMessage(event.getMessage().getText());
 	}
-
+	
 	@EventMapping
 	public StickerMessage handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
 		log.info("event: " + event);
 		StickerMessageContent message = event.getMessage();
-		slackService.sendTextMessage(message.getPackageId() + ":" + message.getStickerId());
-		return new StickerMessage("1", "10");
+		String packageId = message.getPackageId();
+		String stickerId = message.getStickerId();
+		slackService.sendTextMessage(packageId + ":" + stickerId);
+		if(stickerMap.containsKey(packageId)) {
+			// defined packageId
+			return new StickerMessage(packageId, stickerMap.get(packageId));
+		} else {
+			// sorry sticker
+			return new StickerMessage("2", "38");
+		}
 	}
 
 	@EventMapping
