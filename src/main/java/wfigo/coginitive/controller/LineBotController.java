80b9d209a675250c37c3613d2215a20d6f89357f
wfigo7 <wfigo.7@gmail.com>
2017-12-13 00:05:14 +0900

    テキストメッセージでスタンプのパッケージIDとスタンプIDを送信すると対応するスタンプを返す

--- src/main/java/wfigo/coginitive/controller/LineBotController.java
+++ src/main/java/wfigo/coginitive/controller/LineBotController.java
@@ -9,6 +9,7 @@ import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
@@ -27,9 +28,15 @@ public class LineBotController {
 	private Map<String, String> stickerMap = ImmutableMap.of("1", "13", "2", "175", "3", "191", "4", "276");
 
 	@EventMapping
-	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+	public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
 		log.info("event: " + event);
+		String text = event.getMessage().getText();
 		slackService.sendTextMessage(event.getMessage().getText());
+		// sticker pattern message ex. ${packageId}:${stickerId}
+		if(text.indexOf(":") != -1) {
+			String[] textArray = text.split(":");
+			return new StickerMessage(textArray[0], textArray[1]);
+		}
 		return new TextMessage(event.getMessage().getText());
 	}
 	
