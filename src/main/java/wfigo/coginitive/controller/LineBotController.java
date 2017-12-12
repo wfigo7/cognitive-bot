461f923f62188b47ec146f9ddd67295b52e823f3
wfigo7 <wfigo.7@gmail.com>
2017-12-12 23:28:45 +0900

    スタンプメッセージに対応

--- src/main/java/wfigo/coginitive/controller/LineBotController.java
+++ src/main/java/wfigo/coginitive/controller/LineBotController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
@@ -24,6 +26,14 @@ public class LineBotController {
 		log.info("event: " + event);
 		slackService.sendTextMessage(event.getMessage().getText());
 		return new TextMessage(event.getMessage().getText());
+	}
+
+	@EventMapping
+	public StickerMessage handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
+		log.info("event: " + event);
+		StickerMessageContent message = event.getMessage();
+		slackService.sendTextMessage(message.getPackageId() + ":" + message.getStickerId());
+		return new StickerMessage("1", "10");
 	}
 
 	@EventMapping
